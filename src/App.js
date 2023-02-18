edce67fb893e8b00173748db43f735486de2a4a3
Heidi Slojewski <heidi.slojewski@disney.com>
2023-02-18 11:20:04 -0800

    setup basic app router and gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import './App.css';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const React = require('react'),
+      { useState, useEffect } = require('react');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          {/* <Route path="resume" element={<Resume />} /> */}
+        </Route>
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+  // Intro
+  // Projects/Categories list
+  // Contact/Links
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
