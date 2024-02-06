9c4472eb812dc5be2f2efbbe64b863854d22a09b
hslojewski <heidi.slojewski@gmail.com>
2024-02-05 23:26:10 -0800

    add more project data, install social embed plugin

--- src/Project.js
+++ src/Project.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import parse from 'html-react-parser';
 import { Link } from "react-router-dom";
+import { InstagramEmbed } from 'react-social-media-embed';
 
 class Project extends React.Component {
 
-  componentWillMount() {
+  componentDidMount() {
     const {
       getProjectData,
       projectPath = ''
@@ -13,12 +14,12 @@ class Project extends React.Component {
   }
 
   render() {
-    const { data = {}, tags = {}, title = "" } = this.props;
+    const { projectPath = '', data = {}, tags = {}, title = "" } = this.props;
 
     return (
       <div className="content">
         <Link to="/projects">Back to Projects</Link>
-        <h1>Project: {title}</h1>
+        <h1>{title}</h1>
         <p>{data.description}</p>
         <p>
           <strong>Tools & Skills: </strong>
@@ -51,11 +52,21 @@ class Project extends React.Component {
                 <button className={section.button.classes} src={section.button.url}>{section.button.title}</button>
               }
               {section.images && section.images.map(image => {
-                return(<img src={[process.env.PUBLIC_URL, image.src].join("/")} alt={image.alt} className={image.classes} />);
+                return(
+                  <span>
+                    <img src={[process.env.PUBLIC_URL, image.src].join("/")} alt={image.alt} className={image.classes} />
+                    {image.caption && <caption>{image.caption}</caption>}
+                  </span>
+                );
                 }
               )}
               {section.video &&
                 <iframe src={section.video} width="100%" height="400" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+              }
+              {section.instagram_embed &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <InstagramEmbed url={section.instagram_embed} width={550} />
+              </div>
               }
             </div>
           );
